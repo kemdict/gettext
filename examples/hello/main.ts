f048624ca641987692a7d6ec4cf15e5f1eedeab6
Kisaragi Hiu <mail@kisaragi-hiu.com>
2025-12-27 22:45:56 +0900

    hello: extraction and sprintf

--- examples/hello/main.ts
+++ examples/hello/main.ts
@@ -1,7 +1,26 @@
 import Gettext from "@kemdict/gettext";
+import { po } from "gettext-parser";
 import { parseArgs } from "node:util";
+import fs from "node:fs";
+import path from "node:path";
+import { sprintf } from "sprintf-js";
 
-const helptext = `
+function loadTranslations(gt: Gettext) {
+    const podir = path.join(import.meta.dirname, "po");
+    for (const file of fs.readdirSync(podir)) {
+        if (file.endsWith(".po")) {
+            const translations = po.parse(fs.readFileSync(file));
+            gt.addTranslations(file.slice(0, -3), "messages", translations);
+        }
+    }
+}
+const gt = new Gettext();
+loadTranslations(gt);
+gt.setLocale("zh_TW");
+const _ = gt.gettext.bind(gt);
+const ngettext = gt.ngettext.bind(gt);
+
+const helptext = _(`
 @kemdict/gettext hello example - show hello world
 
 (The name is inspired by GNU Hello, which has the same purpose.)
@@ -11,7 +30,7 @@ Options:
 --upper: Upcase message before showing
 --lower: Downcase message before showing
 --help: Show help (this message)
-`;
+`);
 
 const parsedArgs = parseArgs({
     allowPositionals: true,
@@ -25,8 +44,15 @@ const parsedArgs = parseArgs({
 if (parsedArgs.values.help) {
     console.log(helptext);
 } else {
-    console.log("Hello world!");
+    console.log(_("Hello world!"));
     console.log(
-        `You provided ${parsedArgs.positionals.length} positional arguments.`,
+        sprintf(
+            ngettext(
+                `You provided %s positional argument.`,
+                `You provided %s positional arguments.`,
+                parsedArgs.positionals.length,
+            ),
+            parsedArgs.positionals.length,
+        ),
     );
 }
