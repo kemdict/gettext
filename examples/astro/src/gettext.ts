35b812164b4fb3f4cb9cfcd49995a209f5f83b47
Kisaragi Hiu <mail@kisaragi-hiu.com>
2026-02-02 23:41:08 +0900

    it actually does work

--- examples/astro/src/gettext.ts
+++ examples/astro/src/gettext.ts
@@ -3,6 +3,7 @@ import zh_TW from "./po/zh_TW.po";
 
 export const gt = new Gettext({
     translations: {
-        zh_TW,
+        // TODO: get rid of domains in this library
+        zh_TW: { messages: zh_TW },
     },
 });
