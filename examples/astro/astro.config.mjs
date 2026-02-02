1dac29ba2394ea251cbcd4d685f1c0971cab93b3
Kisaragi Hiu <mail@kisaragi-hiu.com>
2026-02-02 23:31:07 +0900

    astro example: start trying to use the rollup plugin

--- examples/astro/astro.config.mjs
+++ examples/astro/astro.config.mjs
@@ -1,3 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-export default defineConfig({});
+import { po, mo } from "rollup-plugin-gettext";
+
+export default defineConfig({
+  i18n: {
+    locales: ["en", "zh_TW"],
+    defaultLocale: "en",
+    routing: {
+      prefixDefaultLocale: true,
+      redirectToDefaultLocale: true,
+    },
+  },
+  vite: { plugins: [po(), mo()] },
+});
