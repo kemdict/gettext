4a4c43f30b222a6b0a093b34d2a931d55eff044e
Kisaragi Hiu <mail@kisaragi-hiu.com>
2026-02-02 23:00:30 +0900

    I think that's a working plugin?

--- packages/rollup-plugin-gettext/index.ts
+++ packages/rollup-plugin-gettext/index.ts
@@ -1,7 +1,64 @@
-// TODO: get plugin types in here
+import { createFilter, dataToEsm, FilterPattern } from "@rollup/pluginutils";
+import {
+    type GetTextPoParserOptions,
+    po as poParser,
+    mo as moParser,
+} from "gettext-parser";
 
-export function po() {
+interface MoPluginOptions {
+    include?: FilterPattern;
+    exclude?: FilterPattern;
+    defaultCharset?: string;
+}
+
+interface PoPluginOptions {
+    include?: FilterPattern;
+    exclude?: FilterPattern;
+    parserOptions?: GetTextPoParserOptions;
+}
+
+export function po(options: PoPluginOptions = {}) {
+    const filter = createFilter(options?.include, options?.exclude);
     return {
-        name: "gettext-po",
+        name: "po",
+        transform(code: string, id: string) {
+            if (!/\.po$/.test(id) || !filter(id)) return;
+            try {
+                const parsed = poParser.parse(code, options.parserOptions);
+                return {
+                    code: dataToEsm(parsed, {
+                        preferConst: true,
+                        compact: true,
+                        namedExports: false,
+                    }),
+                    map: { mappings: "" },
+                };
+            } catch (e) {
+                return null;
+            }
+        },
+    };
+}
+
+export function mo(options: MoPluginOptions = {}) {
+    const filter = createFilter(options?.include, options?.exclude);
+    return {
+        name: "mo",
+        transform(code: string, id: string) {
+            if (!/\.mo$/.test(id) || !filter(id)) return;
+            try {
+                const parsed = moParser.parse(code, options.defaultCharset);
+                return {
+                    code: dataToEsm(parsed, {
+                        preferConst: true,
+                        compact: true,
+                        namedExports: false,
+                    }),
+                    map: { mappings: "" },
+                };
+            } catch (e) {
+                return null;
+            }
+        },
     };
 }
