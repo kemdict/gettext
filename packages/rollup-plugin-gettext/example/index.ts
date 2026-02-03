462e390d4a3b309a8c9d4cdc25d0d16b8cf8f864
Kisaragi Hiu <mail@kisaragi-hiu.com>
2026-02-03 20:50:57 +0900

    some code
    npx vite build now fails at the expected point

--- packages/rollup-plugin-gettext/example/index.ts
+++ packages/rollup-plugin-gettext/example/index.ts
@@ -1,6 +1,14 @@
+import Gettext, { guessEnvLocale } from "@kemdict/gettext";
+
 import poTranslations from "po:./po";
 import moTranslations from "mo:./mo";
 import onePo1 from "po:./po/zh_TW.po";
 import onePo2 from "./po/zh_TW.po";
 import oneMo1 from "mo:./mo/zh_TW.mo";
 import oneMo2 from "./mo/zh_TW.mo";
+
+const { _ } = new Gettext({ translations: poTranslations }).bindLocale(
+    guessEnvLocale(),
+);
+
+console.log(_("Hello!"));
