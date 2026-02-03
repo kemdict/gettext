b6e640ff8953ede6d82fb60a92f32f8bcb666d2e
Kisaragi Hiu <mail@kisaragi-hiu.com>
2026-02-03 20:17:05 +0900

    the filter prop is a rollup 4.38+ feature

--- packages/rollup-plugin-gettext/index.ts
+++ packages/rollup-plugin-gettext/index.ts
@@ -1,7 +1,7 @@
 import path from "node:path";
 import fs from "node:fs/promises";
 
-import { createFilter, dataToEsm } from "@rollup/pluginutils";
+import { dataToEsm } from "@rollup/pluginutils";
 import {
     po as poParser,
     mo as moParser,
