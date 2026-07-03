7b86ea46504add55edc0622887e6572c12116fff
Kisaragi Hiu <mail@kisaragi-hiu.com>
2026-07-04 05:50:52 +0900

    trying it on astro ast

--- playgrounds/extractor/blanker.ts
+++ playgrounds/extractor/blanker.ts
@@ -1,26 +1,10 @@
 // playground for extracting function calls in a way similar to ts-blank-space
 
-import { parse } from "svelte/compiler";
+import { parse as parseSvelte } from "svelte/compiler";
+import { parse as parseAstro } from "@astrojs/compiler-rs";
 import { walk } from "estree-walker";
-import type { Node, CallExpression, SimpleCallExpression } from "estree";
+import type { Node, CallExpression } from "estree";
 
-const source = `
-<script>
-const { _ } = gt.bindLocale("zh_TW")
-_("hello")
-// TRANSLATORS: This comment should be included
-const message = _("world")
-// xgettext: no-space-ellipsis-check
-const message2 = _("worldd")
-const message3 = _("worldd")
-</script>
-
-<div class={_(\`I don't think this works\`)}>{_("another message")}</div>
-<div>{_(
-  // TRANSLATORS: I expect this to be kept
-  "foo"
-)}</div>
-`;
 type WithRange<T> = T & { start: number; end: number };
 
 /** Return an array of all CallExpressions in `ast`. */
@@ -62,18 +46,56 @@ function getSpace(input: string, start: number, end: number): string {
     return out;
 }
 
-function test() {
+function transform(input: string, node: Node) {
     let previousEnd = 0;
     let out = "";
-    for (const expr of getCallExpressions(
-        parse(source, { modern: true }) as unknown as Node,
-    )) {
+    for (const expr of getCallExpressions(node)) {
         const start = Math.max(expr.start, previousEnd);
         const end = expr.end;
         // replace each *codepoint* with a space in the output
-        out += getSpace(source, previousEnd, start);
-        out += source.slice(start, end);
+        out += getSpace(input, previousEnd, start);
+        out += input.slice(start, end);
         previousEnd = end;
     }
-    console.log(out);
+    return out;
 }
+
+const sourceSvelte = `
+<script>
+const { _ } = gt.bindLocale("zh_TW")
+_("hello")
+// TRANSLATORS: This comment should be included
+const message = _("world")
+// xgettext: no-space-ellipsis-check
+const message2 = _("worldd")
+const message3 = _("worldd")
+</script>
+
+<div class={_(\`I don't think this works\`)}>{_("another message")}</div>
+<div>{_(
+  // TRANSLATORS: I expect this to be kept
+  "foo"
+)}</div>
+`;
+transform(
+    sourceSvelte,
+    parseSvelte(sourceSvelte, { modern: true }) as unknown as Node,
+);
+const sourceAstro = `
+---
+const { _ } = gt.bindLocale("zh_TW")
+_("hello")
+// TRANSLATORS: This comment should be included
+const message = _("world")
+// xgettext: no-space-ellipsis-check
+const message2 = _("worldd")
+const message3 = _("worldd")
+---
+
+<div class={_(\`I don't think this works\`)}>{_("another message")}</div>
+<div>{_(
+  // TRANSLATORS: I expect this to be kept
+  "foo"
+)}</div>
+`;
+transform(sourceAstro, parseAstro(sourceAstro).ast as Node);
